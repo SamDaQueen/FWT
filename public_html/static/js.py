a6456c3fd2f8ae7ead0cdbc8713699821c0c0f5a
antgad <37169203+antgad@users.noreply.github.com>
2019-12-02 21:11:22 +0530

    minor updates

--- public_html/static/js.py
+++ public_html/static/js.py
@@ -1,8 +1,12 @@
 import json
 
-data = [{"title": "Shyam Varshik Mahotsav", "start": "2019-12-15T09:00", "end": "2019-12-15T23:00"},
- {"title": "Shyam Satsang Mandal Website Launch", "start": "2019-12-15T20:00", "end": "2019-12-15T20:30"},
- {"title": "Shukla Paksh Ekadashi", "start": "2019-08-11T08:00", "end": "2019-08-11T22:00"}]
-
+data = [      
+      {
+        'title':'Shyam Varshik Mahotsav',
+        'start': '2019-12-15T09:00',
+        'end': '2019-12-15T23:00'
+      },
+    ]
+print("file run")
 with open('data.json', 'w') as outfile:
     json.dump(data, outfile)
